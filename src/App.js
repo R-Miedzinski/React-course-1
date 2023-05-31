2af39526b5d707b688c606adb4c1e7f5f8cd8da4
R-Miedzinski <radek.miedzinski@gmail.com>
2023-05-31 19:43:21 +0200

    Adding database integration

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,46 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import useRequest from "../../Hooks/useRequest";
 // import MealItemForm from "./MealItem/MealItemForm";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 export default function AvailableMeals(props) {
-  const mealsList = DUMMY_MEALS.map((meal) => {
+  const [dataMealsList, setDataMealsList] = useState([]);
+  const {
+    isLoading,
+    error: mealsHasError,
+    sendRequest: sendMealsRequest,
+  } = useRequest();
+
+  useEffect(() => {
+    const transformMeals = (data) => {
+      if (!data) {
+        throw new Error("No meals to load");
+      }
+
+      const loadedMeals = [];
+
+      for (const meal in data) {
+        loadedMeals.push(data[meal]);
+      }
+
+      setDataMealsList(loadedMeals);
+    };
+
+    const result = sendMealsRequest(
+      {
+        url: "https://react-course-c6f1b-default-rtdb.europe-west1.firebasedatabase.app/meals.json",
+      },
+      transformMeals
+    );
+
+    // console.log(result);
+
+    // setDataMealsList(result);
+  }, []);
+
+  // console.log(dataMealsList);
+
+  console.log(isLoading, mealsHasError);
+
+  const mealsList = dataMealsList.map((meal) => {
     return <MealItem key={meal.id} id={meal.id} meal={meal}></MealItem>;
   });
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {!isLoading && !mealsHasError && <ul>{mealsList}</ul>}
+        {!isLoading && mealsHasError && <p>{mealsHasError}</p>}
       </Card>
       {/* <MealItemForm /> */}
     </section>
